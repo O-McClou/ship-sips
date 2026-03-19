31b8e9569222e664384f433d5ad223cd68475fad
O-McClou <o.mcclou@gmail.com>
2026-03-19 12:08:53 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@
      und braucht den Service Worker nicht.
    ═══════════════════════════════════════════════════════════════════ */
 
-const CACHE_NAME = 'tracker-v52';
+const CACHE_NAME = 'tracker-v53';
 const ASSETS = [
   './index.html'
   /* Keine Icons hier – sie fehlen beim file://-Betrieb (AirDrop).
